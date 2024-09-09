8bec8bd009288870f0f58267c11113b2913fc95b
SMarioMan <SMarioMan@users.noreply.github.com>
2024-09-09 04:29:13 -0400

    Implement functional WebSocket handshake system

--- src/utilities/web-rtc.ts
+++ src/utilities/web-rtc.ts
@@ -1,31 +1,99 @@
-// TODO: Nonfunctional. Needs a total rework. Use rtc-test.astro as reference.
+// TODO: Currently untested.
 
-import { state } from "@utilities/state";
-import { Peer } from "peerjs";
+import { Handshake } from "@utilities/web-socket"
+import "./node_modules/simple-peer/simplepeer.min.js";
 
-export function PeerTest(name: string, target: string): void {
-    const peer = new Peer(name, {debug: 3});
-    const conn = peer.connect(target);
-    conn.on("open", () => {
-        conn.send("hi!");
-    });
-    conn.on("error", (err) => {
-        console.log(err);
-      });
-    peer.on("connection", (conn) => {
-        conn.on("data", (data) => {
-            console.log(data);
-        });
-        conn.on("open", () => {
-            conn.send("hello!");
-        });
-    });
-    peer.on('open', function(id) {
-        console.log('My peer ID is: ' + id);
-    });
+// A comprehensive list of commands to accommodate:
+// getName: Handles the process of getting a username for a client.
+//          The server requests this upon a successful connection.
+//          The client uses this to provide the response.
+//          The server also sends a command with this, alongside an "error", if the name is invalid.
+// TODO: characterSheet: The server provides the player's current character sheet data.
+// TODO: ability: The client provides a new ability in the "strength" or "weakness" fields.
+//          The server also sends a command with this, alongside an "error", if the ability is invalid.
+// TODO: setState: Sent by the server to inform the client to change state. Valid states come from enum GameState.
+
+// TODO: Consider marking the first peer to connect as a "leader", which permits them to send additional commands. If this happens, be sure to send a command to inform them of this, so the client can unlock the extra UI associated with that.
+
+export class WebRTCHost {
+    private peers: { [key: string]: SimplePeer } = {};
+    private handshake: Handshake | null = null;
+    constructor() { }
+    public OpenConnections(): void {
+        this.handshake = new Handshake()
+        function customHandler(event: MessageEvent<string>) {
+            console.log(`Received message from client: ${event.data}`);
+            // TODO: Negotiation takes more than naively creating a peer. You need to handle things based on room code, for instance. Perhaps the WebSocket server should handle a lot of this though?
+            // TODO: I intend to use a standard format for all messages, which will include additional auth. Ideally, the WebSocket server will handle message filtering, but we can receive the data here and do things with it to reject certain handshakes automatically.
+
+            const peer: SimplePeer = new SimplePeer({
+                initiator: false, // The host always waits for clients to ask them to connect.
+                trickle: false,
+            });
+            // Handle errors.
+            peer.on("error", (err) => console.log("error", err));
+            // Handle handshakes.
+            peer.on("signal", (data) => {
+                console.log("SIGNAL", JSON.stringify(data));
+                // TODO: Include extra identifying info for for WebSocket routing purposes.
+                this.handshake.send(JSON.stringify(data))
+            });
+            // Establish connection.
+            peer.on("connect", () => {
+                console.log("CONNECT");
+                // Request a name.
+                const request: object = {
+                    command: "GetName"
+                }
+                peer.send(JSON.stringify(request))
+            });
+            // Close connection.
+            // TODO: Is it appropriate to close the connection, or might they return later and reuse this peer?
+            peer.on('close', () => {
+                console.log("CLOSE");
+                Object.keys(this.peers).forEach((key) => {
+                    if (this.peers[key] === peer) {
+                        delete this.peers[key];
+                        return;
+                    }
+                });
+            })
+            // Receive data.
+            // TODO: Handle data reception here (abilities, name, auth, etc.)
+            // TODO: Always check the current game state to determine if a type of message is valid. Otherwise reject them.
+            // This will interface with the game by directly modifying the game state.
+            // I don't really like it, but that also means that many LLM inference steps should be put here too. I think I should separate each command into functions that are called from another file, to keep this whole thing from getting overwhelmingly bloated.
+            // I should figure out how to end the client round communication once time runs out and figure out how to delay until any outstanding inference completes.
+            peer.on("data", (data) => {
+                console.log("data: " + data);
+                if(!("command" in data)) {
+                    console.log("Client did not provide a command. Ignoring.");
+                    return;
+                }
+                if (data.command === "GetName") {
+                    // TODO: Is there a way to validate against a schema or something? This might make things cleaner.
+                    if(!("name" in data)) {
+                        console.log("Client provided malformed data. Ignoring.");
+                        return;
+                    }
+                    if (data.name in this.peers) {
+                        console.log("Peer requested an existing name. Rejecting.");
+                        const request: object = {
+                            command: "GetName",
+                            error: "duplicate"
+                        }
+                        peer.send(request)
+                        return
+                    } else {
+                        // Associate the peer to the name.
+                        this.peers[data.name] = peer;
+                    }
+                }
+            });
+            // Respond to the handshake.
+            peer.signal(JSON.parse(event.data))
+        }
+        this.handshake.setHandleMessage(customHandler)
+        this.handshake.connect()
+    }
 }
-
-// export function Send(msg: string): void {
-//     conn.send(msg)
-//     return
-// }
---
+++ src/utilities/web-socket.ts
@@ -0,0 +1,64 @@
+import { state } from "@utilities/state";
+
+export class Handshake {
+    private socket: WebSocket | null = null;
+    private handleOpen: () => void;
+    private handleMessage: (event: MessageEvent<string>) => void;
+    private handleClose: (event: CloseEvent) => void;
+    private handleError: (error: Event) => void;
+
+    constructor() {
+        this.handleOpen = () => {
+            console.log('Connected to the WebSocket server');
+        };
+
+        this.handleMessage = (event: MessageEvent<string>) => {
+            console.log(`Received message from server: ${event.data}`);
+        };
+
+        this.handleClose = (event: CloseEvent) => {
+            console.log(`Disconnected from WebSocket server. Code: ${event.code}, Reason: ${event.reason}`);
+        };
+
+        this.handleError = (error: Event) => {
+            console.error('WebSocket error:', error);
+        };
+    }
+
+    public connect(): void {
+        if (!this.socket) {
+            // TODO: Make this customizable, as needed, to allow self-hosting.
+            // TODO: Add support to negotiate on room codes.
+            this.socket = new WebSocket('ws://localhost:8080');
+            this.setupSocketEvents();
+        }
+    }
+
+    private setupSocketEvents(): void {
+        this.socket!.onopen = this.handleOpen;
+        this.socket!.onmessage = this.handleMessage;
+        this.socket!.onclose = this.handleClose;
+        this.socket!.onerror = this.handleError;
+    }
+
+    // Used to change the handler from the default.
+    public setHandleMessage(newHandler: (event: MessageEvent<string>) => void): void {
+        this.handleMessage = newHandler;
+    }
+
+    // TODO: We can use this to communicate with the WebSocket server and "claim" a room code for our use.
+    // TODO: Every peer should provide their "roomID" and identify as a "host" or "client", for routing purposes, even if it's only to handshake for WebRTC.
+    // TODO: To handle reconnects, a secret should be provided by the server to re-authenticate a client.
+    public send(msg: string | ArrayBufferLike | Blob | ArrayBufferView): void {
+        if (this.socket && this.socket.readyState === WebSocket.OPEN) {
+            this.socket.send(msg);
+        }
+    }
+
+    public disconnect(): void {
+        if (this.socket) {
+            this.socket.close();
+            this.socket = null;
+        }
+    }
+}
